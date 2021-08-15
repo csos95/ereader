d87acd48f57d50943bf07f5516d0bc83323108f8
csos95 <csoscss@gmail.com>
2021-08-15 05:57:16 +0000

    start working on library listing
    FossilOrigin-Name: dbdb6cb3c63b5910149716e191c1297906b2799e1a5d7c77b257ce37bbb57561

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,26 @@ mod library;
 mod scan;
 
 use sqlx::SqlitePool;
+use cursive::{Cursive, CursiveExt};
+use cursive::views::SelectView;
 
 #[async_std::main]
 async fn main() {
     let pool = SqlitePool::connect("ereader.sqlite").await.unwrap();
 
     scan::scan(&pool, "epub").await.unwrap();
+
+    let books = library::get_books(&pool).await.unwrap();
+
+    let mut siv = Cursive::new();
+
+    let mut view = SelectView::new().h_align(cursive::align::HAlign::Left);
+
+    for book in &books {
+        view.add_item(book.title.clone(), book.id);
+    }
+
+    siv.add_layer(cursive::views::Dialog::around(view).title("Library"));
+    siv.add_global_callback('q', |s| s.quit());
+    siv.run();
 }
